6a8271508495ae8bc40ad6e7f6f405827cc6d0fc
Emma Jiang <emmajiang@berkeley.edu>
2019-03-17 11:15:31 -0700

    assets edited

--- App.js
+++ App.js
@@ -1,56 +1,73 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, AppRegistry, Image, Button, Alert } from 'react-native';
-
-
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-  	let display = this.state.isShowingText ? this.props.text : ' ';
-    return (
-    	<Text style={styles.bigblue}>{display}</Text>
-    );
-  }
-}
-export default class BlinkApp extends Component {
-  render() {
-  	return (
-      <View style={{alignItems: 'center'}, {flex : 1}}>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
-        <Button
-        onPress={() => {
-        	Alert.alert('You tapped the button!');
-        }}
-        title="Press Me"
-        />
-      </View>
-    );
-  }
-}
+import React from 'react';
+import {
+  Blink,
+  Image,
+  ImageBackground,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
 
 const styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
+  bigBlue: {
+    color: 'white',
     fontWeight: 'bold',
     fontSize: 30,
   },
-  red: {
-    color: 'red',
-  },
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+  render() {
+    return (
+      <ImageBackground source={
+          require('./assets/images/fp_back.png')
+        }
+        style={{width: '100%', height: '100%'}}>
 
+        <View style={{flex: 1, flexDirection:'row', justifyContent: 'space-evenly', alignItems: 'center', paddingTop: 380}}>
+
+          <Image source={
+            require('./assets/images/fp_camera.png')
+          }
+          style={{width: 150, height: 150}}>
+          </Image>
+
+        </View>
+
+
+        <View style={{flex: 2, flexDirection:'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: 80, paddingLeft: 15, paddingRight: 15}}>
+
+          <Image source={
+            require('./assets/images/fp_gallery.png')
+          }
+          style={{width: 120, height: 120}}>
+          </Image>
+
+          <Image source={
+            require('./assets/images/fp_index.png')
+          }
+          style={{width: 120, height: 120}}>
+          </Image>
+
+        </View>
+
+        <View style={{flex: 3, flexDirection:'row', justifyContent: 'space-around', alignItems: 'center', paddingBottom: 180}}>
+
+          <Image source={
+            require('./assets/images/fp_search.png')
+          }
+          style={{width: 120, height: 120}}>
+          </Image>
+
+        </View>
+      </ImageBackground>
+    );
+  }
+}
