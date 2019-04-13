048c1e73bf6e257a9b8d01eca305123f8b77ddac
ryo1024 <ryo10244201@berkeley.edu>
2019-04-12 18:28:31 -0700

    Fixing cameraroll functionality

--- component/CamerarollScreen.js
+++ component/CamerarollScreen.js
@@ -16,7 +16,7 @@ import {
 } from 'react-native';
 import { WebBrowser } from 'expo';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
-import { Cameraroll, Permissions } from 'expo';
+import {Permissions } from 'expo';
 
 class CamerarollScreen extends React.Component {
   static navigationOptions = {
@@ -43,7 +43,7 @@ class CamerarollScreen extends React.Component {
   }
 
   access() {
-    CameraRoll.getPhotos({ first: 1000000 })
+    CameraRoll.getPhotos({ first: 100 })
       .then(res => {
         let photoArray = res.edges;
         this.setState({ showPhotoGallery: true, photoArray: photoArray })
@@ -51,22 +51,39 @@ class CamerarollScreen extends React.Component {
   }
 
   render() {
+    const initial = <TouchableHighlight
+                        onPress={() => this.access()}>
+                        <Image style={{ width: 75, height:75 }}
+                          source={require('../assets/images/fp_gallery.png')} />
+                      </TouchableHighlight>
+                      
+    let rendering;
     if (this.state.showPhotoGallery) {
-      return (
-        <ViewPhotos
-          photoArray={this.state.photoArray} />
-      )
+      rendering = <ScrollView>
+                      {this.state.photoArray.map((p, i) => {
+                      return (
+                        <TouchableOpacity key={i} onPress={() => { {this.props.navigation.navigate('Predictor', {image: p.node.image.uri})} }}>
+                            <Image
+                              key={i}
+                              style={{
+                                width: 300,
+                                height: 300,
+                              }}
+                              source={{ uri: p.node.image.uri }}
+                            />
+                        </TouchableOpacity>
+                      );
+                    })}
+                    </ScrollView>
+    } else {
+      rendering = initial
     }
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#4C3D35' }}>
-        <TouchableHighlight
-          onPress={() => this.access()}>
-          <Image style={{ width: 75, height:75 }}
-            source={require('../assets/images/fp_gallery.png')} />
-        </TouchableHighlight>
-        <Text style= {{ fontFamily: 'Noteworthy-Bold', fontSize: 28, color:'white' }}>
-          tap to load images
-        </Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#4C3D35' }}> 
+          {rendering} 
+          <Text style= {{ fontFamily: 'Noteworthy-Bold', fontSize: 28, color:'white' }}>
+           tap to load images
+          </Text>
       </View>
     );
   }
